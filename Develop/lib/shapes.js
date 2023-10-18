7f22783b355c8fcf986bef75a478d272541a1103
Ash <coreyvladesov2023@u.northwestern.edu>
2023-10-18 18:00:40 -0500

    made some changes then reverted back

--- Develop/lib/shapes.js
+++ Develop/lib/shapes.js
@@ -22,7 +22,7 @@ class Triangle extends Shape{
         super(color);
     }
     render() {
-        return `<polygon points="150,18 244,182 56,182" fill="${this.color}" />`;
+        return `<polygon points="150,18 244,182 56,182" fill="${this.color}"/>`;
     }
 }
 
